f7b8176d57298fce1ad0b4399c4bbbe37b838127
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-11-14 14:57:16 +0530

    Add scope and infofields so that the site can capture those fields

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,7 +233,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"], scope: 'email, public_profile', info_fields: 'email, first_name, last_name'
+
+  # config.omniauth :facebook, "app id", "app secret", scope: 'email', info_fields: 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
