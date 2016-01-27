273bf30e42b2cd059c6236f050db0dc7705af9c8
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-01-27 16:00:45 +0530

    comment out default-pending tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,49 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do render_views
-
-  describe "GET #home" do
-    it "returns http success" do
-      skip
-      # get :home
-      # expect(response).to have_http_status(:success)
-      # expect(response.body).to have_link('About Us', href: about_path)
-      # expect(response.body).to have_link('FAQ', href: faq_path)
-      # expect(response.body).to have_link('Contact', href: contact_path)
-      # expect(response.body).to have_link('Legal', href: legal_path)
-    end
-  end
-
-  describe "GET #about" do
-    it "returns http success" do
-      skip
-      # get :about
-      # expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #faq" do
-    it "returns http success" do
-      skip
-      # get :faq
-      # expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #contact" do
-    it "returns http success" do
-      skip
-      # get :contact
-      # expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #legal" do
-    it "returns http success" do
-      skip
-      # get :legal
-      # expect(response).to have_http_status(:success)
-    end
-  end
-
+  #
+  # describe "GET #home" do
+  #   # it "returns http success" do
+  #   #   skip
+  #   #   # get :home
+  #   #   # expect(response).to have_http_status(:success)
+  #   #   # expect(response.body).to have_link('About Us', href: about_path)
+  #   #   # expect(response.body).to have_link('FAQ', href: faq_path)
+  #   #   # expect(response.body).to have_link('Contact', href: contact_path)
+  #   #   # expect(response.body).to have_link('Legal', href: legal_path)
+  #   # end
+  # end
+  #
+  # describe "GET #about" do
+  #   it "returns http success" do
+  #     skip
+  #     # get :about
+  #     # expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #faq" do
+  #   it "returns http success" do
+  #     skip
+  #     # get :faq
+  #     # expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #contact" do
+  #   it "returns http success" do
+  #     skip
+  #     # get :contact
+  #     # expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #legal" do
+  #   it "returns http success" do
+  #     skip
+  #     # get :legal
+  #     # expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
 end
