2f953fc694c676458aef4f32afa1e2d706337668
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-02-09 13:13:32 +0530

    Makes wallet work

--- app/controllers/transaction_logs_controller.rb
+++ app/controllers/transaction_logs_controller.rb
@@ -1,11 +1,14 @@
 class TransactionLogsController < ApplicationController
 
   def create
-    byebug
     if current_user.wallet.nil?
+      new_wallet = Wallet.create(user_id: current_user.id, amount: BigDecimal.new(params[:transaction_logs][:amount])
+ )
+    else
+      new_wallet_amount = current_user.wallet.amount - BigDecimal.new(params[:transaction_logs][:amount])
+      current_user.wallet.update(amount: new_wallet_amount)
     end
-    current_user.wallet = current_user.wallet- params[:transaction_logs][:amount]
-    redirect_to root_path, notice: "Payment has been successful"
+    redirect_to root_path, notice: "Payment has been successful. Your current wallet balance is #{current_user.wallet.amount}USD"
   end
 
 private
