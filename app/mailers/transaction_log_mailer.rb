b4053d38b01f2966dc7fa3e48d2edd332af5569c
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-02-11 19:55:32 +0530

    Send email to User and shenil

---
+++ app/mailers/transaction_log_mailer.rb
@@ -0,0 +1,16 @@
+class TransactionLogMailer < ApplicationMailer
+  default to: 'alphabeatsuploads@gmail.com', from: 's@alphabeatsmusic.com'
+
+  def transaction_email(user, order)
+    @user = user
+    @order = order.reload
+    @transaction_log = order.reload.transaction_log
+    @album= order.order_items.last.album
+    byebug
+
+    # @order = transactionlog.reload.order
+    # @wallet_balance = @user.reload.wallet.amount
+    # @album = transactionlog.reload.order.order_items.last.album
+    mail(to: @user.email, subject: 'Details of the album you paid')
+  end
+end
