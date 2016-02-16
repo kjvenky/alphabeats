8812454f762cc0325cf8e7f05a86854171d2aade
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-02-16 07:23:06 +0530

    changed t.references

--- db/migrate/20160103104455_create_albums.rb
+++ db/migrate/20160103104455_create_albums.rb
@@ -2,7 +2,7 @@ class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
       t.string :album_name
-      t.references :users, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
