60d1b4a2701a56b5cb961d40c15344a2d83686b3
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-02-16 07:01:45 +0530

    Modifies create_albums to make the migration works

--- db/migrate/20160103104455_create_albums.rb
+++ db/migrate/20160103104455_create_albums.rb
@@ -2,9 +2,11 @@ class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
       t.string :album_name
-      t.references :musician, index: true, foreign_key: true
+      t.integer :musician_id
 
       t.timestamps null: false
+      add_foreign_key :albums, :users, column: :musician_id
+      add_index :albums, :musician_id
     end
   end
 end
