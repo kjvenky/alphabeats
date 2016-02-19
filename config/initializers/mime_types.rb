a9d9838fe21b3675909676472e6278dea1c0ddcd
shenils <shenil@bu.edu>
2016-02-18 20:04:54 -0500

    mobile content added

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,4 +2,5 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.register "text/richtext", :mobile
+
+Mime::Type.register "text/html", :mobile
