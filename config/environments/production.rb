9704349716ac1fd8a03a3aba0356888428af98bf
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-03-05 10:48:45 +0530

    add environment variable for host and create_new_album

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Mailer configuration
-  config.action_mailer.default_url_options = { host: 'alphabeatsmusic.com' }
+  # config.action_mailer.default_url_options = { host: 'alphabeatsmusic.com' }
+  config.action_mailer.default_url_options = { host: "#{ENV['HOST']}" }
 
   # config.serve_static_assets = true
   config.serve_static_files = true
