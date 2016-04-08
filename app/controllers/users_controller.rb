e89763b917167324ba6672d60393ff1a8db8ae6a
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-04-08 19:00:23 +0530

    Edit Path for users
    Adds edit action, page, route
    and yet to add visibility based on abilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,17 @@ class UsersController < ApplicationController
     # @song = current_user.songs.find(params[:id])
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to song_path(@user), notice: "The user #{@user.first_name} has been updated"
+      redirect_to user_path(@user), notice: "The user #{@user.first_name} has been updated"
     else
       render "show", notice: "The user #{@user.first_name} could not be updated"
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @song_count = @user.songs.count
+  end
+
 private
 
   def user_params
