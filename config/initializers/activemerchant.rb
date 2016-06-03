6ae021ded6199092cc74da0e1fbfcf6c225f77c9
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-06-03 17:51:20 +0530

    Changes in activemerchant initializer to configure for different environments

--- config/initializers/activemerchant.rb
+++ config/initializers/activemerchant.rb
@@ -1,11 +1,15 @@
-
 if Rails.env.test? or Rails.env.cucumber? or Rails.env.development? 
   ActiveMerchant::Billing::Base.mode = :test # change to :production for live
 else
   ActiveMerchant::Billing::Base.mode = :production # change to :production for live
 end
-  ::GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(
+
+if Rails.env.test?
+ ::EXPRESS_GATEWAY = ActiveMerchant::Billing::BogusGateway.new
+else
+  ::EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(
     login: ENV['PAYPAL_USERNAME'],
     password: ENV['PAYPAL_PASSWORD'],
     signature: ENV['PAYPAL_SIGNATURE']
   )
+end
