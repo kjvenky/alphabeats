5af413d62b3a365d7d2f01b6df0459875836c402
shenil <shenil@bu.edu>
2016-06-12 12:16:22 +0530

    change my account icon size

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.test? or Rails.env.cucumber?
+  if Rails.env.test? or Rails.env.cucumber? or Rails.env.development?
     config.storage = :file
     config.enable_processing = false
 
@@ -18,18 +18,18 @@ CarrierWave.configure do |config|
 
   end
 
-  if Rails.env.development?
-    config.storage = :fog
-    config.enable_processing = true
-    config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],       # required
-    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],       # required
-    :region                 => ENV['S3_REGION']  # optional, defaults to 'us-east-1'
-    }
-    config.fog_directory  = ENV['S3_BUCKET']
-    # config.fog_public     = true  
-  end
+  # if Rails.env.development?
+  #   config.storage = :fog
+  #   config.enable_processing = true
+  #   config.fog_credentials = {
+  #   :provider               => 'AWS',       # required
+  #   :aws_access_key_id      => ENV['S3_ACCESS_KEY'],       # required
+  #   :aws_secret_access_key  => ENV['S3_SECRET_KEY'],       # required
+  #   :region                 => ENV['S3_REGION']  # optional, defaults to 'us-east-1'
+  #   }
+  #   config.fog_directory  = ENV['S3_BUCKET']
+  #   # config.fog_public     = true  
+  # end
   
   if Rails.env.production?
     config.storage = :fog
