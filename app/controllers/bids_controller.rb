706d49ea8166310f4851f85c28db228010327b0a
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-06-14 18:00:22 +0530

    Throws a pop up when a bid is created with low balance
    The bootstrap modal shows a button to redirect to wallet page
    
    Additional features:
    show much has to be added
    Add that balance as a preset into the wallet page.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,17 +3,20 @@ class BidsController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @bid = current_user.bids.new(share: params[:share], amount: params[:amount], song_id: params[:song_id], open_status: 1)
-    if @bid.save
-      render "create.js.erb" 
+    if current_user.wallet.amount < BigDecimal.new(params[:amount])
+      render "low_balance.js.erb"
     else
-      redirect_to root_path, :notice=> "unsuccessful bid"
+      @bid = current_user.bids.new(share: params[:share], amount: params[:amount], song_id: params[:song_id], open_status: 1)
+      if @bid.save
+        render "create.js.erb" 
+      else
+        redirect_to root_path, :notice=> "unsuccessful bid"
+      end
     end
 
   end
 
   def update
-    # byebug
       @bid = current_user.bids.find(params[:id])
       @bid.update(share: params[:bid][:share], amount: params[:bid][:amount])
   end
