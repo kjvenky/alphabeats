880c529a60e833ab86f360db61c1025403a12682
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2016-06-28 15:38:33 +0530

    Check for available shares before an offer is created
    Offer controller checks if there are any shares and if
    the shares offered are available under the said users name
    
    Payments controller now no longers logs the payment issues.
    
    If the said user does not have enough share balance the
    low_share_offer.js.erb modal appears.
    
    Additional checks.
    The feature does has not been checked for low share balance yet.
    It has been checked only for if there are any shares which are available

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,11 +4,17 @@ class OffersController < ApplicationController
   before_filter :authenticate_user!
   
   def create
-    @offer = current_user.offers.new(share: params[:share], amount: params[:amount], song_id: params[:song_id], open_status: 1)
-    if @offer.save
-      render "create.js.erb" 
+
+    # if current_user.shareholder_songs.find(song_id: params[:song_id]).nil? || current_user.shareholder_songs.find(song_id: params[:song_id]).share < BigDecimal.new(params[:share])
+    if Shareholder.where(song_id: params[:song_id], user_id: current_user.id).empty? || Shareholder.find(song_id: params[:song_id], user_id: current_user.id).share < BigDecimal.new(params[:share])
+      render "low_share_offer.js.erb"
     else
-      redirect_to root_path, :notice=> "unsuccessful offer"
+      @offer = current_user.offers.new(share: params[:share], amount: params[:amount], song_id: params[:song_id], open_status: 1)
+      if @offer.save
+        render "create.js.erb" 
+      else
+        redirect_to root_path, :notice=> "unsuccessful offer"
+      end
     end
   end
 
